90b58c203176e5df019914c0645aef8e276026a3
vak04 <vak04@DESKTOP-PHME5JL>
2024-03-20 13:51:39 +0200

    Add project files.

---
+++ S2Lab3/Chas.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace S2Lab3
+{
+    public class Chas
+    {
+        private int Znachenna;
+
+        public void AddTime(int time)
+        {
+            try
+            {
+                if (time < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                Znachenna += time;
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Time can't be negative");
+            }
+        }
+
+        public static string ToJSON(int ho, int min, int sec)
+        {
+            string s1 = JsonSerializer.Serialize($"{ho}:{min}:{sec}");
+            return s1;
+        }
+
+        public static string FromJSON (string s1)
+        {
+            string s2 = JsonSerializer.Deserialize<string>(s1);
+            return s2;
+        }
+
+        public void setZnachenna (int i)
+        {
+            try
+            {
+                if (i < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                Znachenna = i;
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Time can't be negative");
+            }
+        }
+
+        public int getZnachenna()
+        {
+            return Znachenna;
+        }
+
+
+        public void WriteH()
+        {
+            Console.WriteLine($"години {Znachenna}");
+        }
+        public void WriteM()
+        {
+            Console.WriteLine($"хвилини {Znachenna}");
+        }
+        public void WriteS()
+        {
+            Console.WriteLine($"секунди {Znachenna}");
+        }
+    }
+}
---
+++ S2Lab3/Program.cs
@@ -0,0 +1,53 @@
+﻿using S2Lab3;
+using System.Text.Json;
+
+try 
+{
+    int x;
+    Console.Write("hours = ");
+    x = int.Parse(Console.ReadLine());
+    int y;
+    Console.Write("minutes = ");
+    y = int.Parse(Console.ReadLine());
+    int z;
+    Console.Write("seconds = ");
+    z = int.Parse(Console.ReadLine());
+
+    Chas hours = new Chas();
+    hours.setZnachenna(x);
+    Chas minutes = new Chas();
+    minutes.setZnachenna(y);
+    Chas seconds = new Chas();
+    seconds.setZnachenna (z); 
+
+    hours.WriteH();
+    minutes.WriteM();
+    seconds.WriteS();
+
+    int xp, yp, zp;
+    Console.Write("додати годин = ");
+    xp = int.Parse(Console.ReadLine());
+    Console.Write("додати хвилин = ");
+    yp = int.Parse(Console.ReadLine());
+    Console.Write("додати секунд = ");
+    zp = int.Parse(Console.ReadLine());
+
+    hours.AddTime(xp);
+    minutes.AddTime(yp);
+    seconds.AddTime(zp);
+
+    hours.WriteH();
+    minutes.WriteM();
+    seconds.WriteS();
+
+   Console.WriteLine(Chas.ToJSON(hours.getZnachenna(), minutes.getZnachenna(), seconds.getZnachenna()));
+   Console.WriteLine(Chas.FromJSON(Chas.ToJSON(hours.getZnachenna(), minutes.getZnachenna(), seconds.getZnachenna())));
+
+   
+}
+catch
+{
+    Console.WriteLine(" syntax error");
+}
+
+
